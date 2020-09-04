1b6517448d6f61292fd4665b8ad7f2fc96dc058d
Kyle M Hall <kyle@bywatersolutions.com>
2020-09-04 15:06:56 -0400

    Finish initial development

--- create_group_tickets.pl
+++ create_group_tickets.pl
@@ -2,8 +2,12 @@
 
 use Modern::Perl;
 
+use Email::Address;
+use Error qw(:try);
 use Getopt::Long::Descriptive;
 use Mojo::DOM;
+use RT::Client::REST::Ticket;
+use RT::Client::REST;
 use WWW::Mechanize;
 
 my ( $opt, $usage ) = describe_options(
@@ -11,6 +15,9 @@ my ( $opt, $usage ) = describe_options(
     [ 'url=s', "Base URL for RT", { required => 1 } ],
     [ 'username|u=s', "RT username", { required => 1 } ],
     [ 'password|p=s', "RT password", { required => 1 } ],
+    [],
+    [ 'subject|s=s', "Ticket subject", { required => 1 } ],
+    [ 'message|m=s', "Ticket message", { required => 1 } ],
     [],
     [ 'verbose|v', "print extra stuff" ],
     [ 'help', "print usage message and exit", { shortcircuit => 1 } ],
@@ -20,6 +27,20 @@ print( $usage->text ), exit if $opt->help;
 my $url = $opt->url;
 my $username = $opt->username;
 my $password = $opt->password;
+
+
+my $rt = RT::Client::REST->new(
+    server  => $url,
+    timeout => 30,
+);
+
+try {
+    $rt->login( username => $username, password => $password );
+}
+catch Exception::Class::Base with {
+    die "problem logging in: ", shift->message;
+};
+
 
 my $groups_url = qq{$url/Admin/Groups/index.html?Format=%27%3Ca%20href%3D%22__WebPath__%2FAdmin%2FGroups%2FModify.html%3Fid%3D__id__%22%3E__id__%3C%2Fa%3E%2FTITLE%3A%23%27%2C%27%3Ca%20href%3D%22__WebPath__%2FAdmin%2FGroups%2FModify.html%3Fid%3D__id__%22%3E__Name__%3C%2Fa%3E%2FTITLE%3AName%27%2C%27__Description__%27%2C__Disabled__&Order=ASC&OrderBy=Name&Page=1&Rows=5000};
 my $group_url = qq{$url/Admin/Groups/Members.html?id=};
@@ -64,10 +85,48 @@ foreach my $group_id ( @group_ids ) {
     my $emails = $dom->find( 'ul li a[href^=/User/Summary.html]' )->to_array;
     my @addresses;
     foreach my $email ( @$emails ) {
-        next if $email->text =~ /^Logged in as/;
-        say $email->text if $opt->verbose;
-        push( @addresses, $email->text );
+        my $text = $email->text;
+
+        next if $text =~ /^Logged in as/;
+
+        say "REQUESTOR: $text" if $opt->verbose;
+        my @addrs = Email::Address->parse( $text );
+        foreach my $addr ( @addrs ) {
+            say "FOUND EMAIL ADDRESS: " . $addr->address if $opt->verbose;
+            push( @addresses, $addr->address );
+        }
+
+        unless ( @addrs ) { # Requestor is an RT username, not an email address
+            if ( $text =~ m/\(.*\)/ ) {
+                my ( $rt_username ) = split(/\(/, $text );
+                say "FOUND RT USERNAME: $rt_username";
+                push( @addresses, $rt_username);
+            }
+        }
     }
 
     $groups->{$group_name} = \@addresses;
+}
+
+foreach my $group_name ( keys %$groups ) {
+    my $group = $groups->{$group_name};
+
+    my $subject = $opt->subject;
+    my $message = $opt->message;
+
+    my @requestors = @$group;
+
+    my $ticket = RT::Client::REST::Ticket->new(
+        rt         => $rt,
+        queue      => "Systems",
+        subject    => $subject,
+        owner      => 'mitch',
+        status     => 'open',
+        requestors => \@requestors,
+    )->store( text => q{} );
+    say "TICKET CREATED: ", $ticket->id if $opt->verbose;
+
+    $ticket->correspond(
+        message => $message,
+    );
 }
